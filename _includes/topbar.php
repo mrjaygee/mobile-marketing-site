e504519fecda26f84ea1071a63f50c9bd25fa28a
Andrew Berkowitz <andrew@teamsnap.com>
2013-02-05 12:33:39 -0800

    Add Google Tag Manager

--- _includes/topbar.php
+++ _includes/topbar.php
@@ -1,3 +1,21 @@
+<script>
+  dataLayer = [{
+    'subdomain': 'mobile-marketing'
+  }];
+</script>
+
+<%= yield :data_layer -%>
+
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WJ3N"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WJ3N');</script>
+<!-- End Google Tag Manager -->
+
 <header>
 <?php if ($page!="home"): ?>
 	<?php include 'main_menu.php'; ?>	
